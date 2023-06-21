4d1784abf0f865b156e8a0b5caa96ba7c17e0684
Bryan Amora <amorabryan@yahoo.com>
2023-06-21 16:27:57 +0000

    Revised big-o-notation

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -12,7 +12,7 @@ function uniqueLinear(words) {
     if (!seen[word]) {
       // 2 * n = O(2n) ~= O(n)
       seen[word] = true; // 2 * n = O(2n) ~= O(n)
-      unique[unique.length] = word; // 3 * n = O(3n) ~= O(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= O(n)
     }
   }
   return unique; // 1 * 1 = O(1)
@@ -26,21 +26,21 @@ function uniqueQuadratic(words) {
     i++ // 2 * n = O(2n) ~= O(n)
   ) {
     const word = words[i]; // 2 * n = O(2n) ~= O(n)
-    let isUnique = true; // 2 * n = O(2n) ~= O(n)
+    let isUnique = true; // 1 * n = O(n)
     for (
       let c = 0; // 1 * n = O(n)
-      c < i; // 2 * n * n = O(2n^2) ~= O(n^2)
+      c < i; // 1 * n * n = O(n^2)
       c++ // 2 * n * n = O(2n^2) ~= O(n^2)
     ) {
       const comparing = words[c]; // 2 * n * n = O(2n^2) ~= O(n^2)
       if (comparing === word) {
         // 1 * n * n = O(n^2)
-        isUnique = false; // _ * _ = O(?)
+        isUnique = false; // 1 * n * n = O(n^2)
       }
     }
     if (isUnique) {
       // 1 * n = O(n)
-      unique[unique.length] = word; // 3 * n = O(3n) ~= O(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= O(n)
     }
   }
   return unique; // 1 * 1 = O(1)
