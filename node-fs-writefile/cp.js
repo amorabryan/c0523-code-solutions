2596f0ec4663cbce8495c5706a5ccff4b1f55644
Bryan Amora <amorabryan@yahoo.com>
2023-07-07 18:22:11 +0000

    Revised cp.js

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -2,11 +2,11 @@ import { writeFile, readFile } from 'node:fs/promises';
 
 async function cp() {
   try {
-    const contents = await readFile(process.argv[2], 'utf8');
-    const copyFile = String(process.argv[3]);
-    await writeFile(copyFile, `${contents}\n`);
+    const contents = await readFile(process.argv[2]);
+    const copyFile = process.argv[3];
+    await writeFile(copyFile, `${contents}`);
   } catch (error) {
-    console.log(`Error: ${error}`);
+    console.log('Error:', error);
   }
 }
 
