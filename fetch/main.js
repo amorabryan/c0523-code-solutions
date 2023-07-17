242c8295ba837b8f0411d1a3ecdaa9edf11371cf
Bryan Amora <amorabryan@yahoo.com>
2023-07-17 20:29:12 +0000

    revised fetch Error

--- fetch/main.js
+++ fetch/main.js
@@ -2,7 +2,7 @@ async function getPokemon() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/4');
     if (!response.ok) {
-      throw new Error('Request failed:', response.status);
+      throw new Error(`Request failed: ${response.status}`);
     }
     const pokemon = await response.json();
     console.log(pokemon);
